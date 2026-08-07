4af426e49944fea8743cc9d188f6190d2b99a77d
saiyeem-abass <abasssayeem7@gmail.com>
2026-08-08 01:48:39 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (68.82%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -3,7 +3,6 @@ public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int count = 0;
         int maxCount = 0;
-
         for (int num : nums) {
             if (num == 1) {
                 count++;
@@ -12,7 +11,6 @@ public:
                 count = 0;
             }
         }
-
         return maxCount;
     }
 };
